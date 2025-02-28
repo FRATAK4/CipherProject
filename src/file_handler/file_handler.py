52d060c9cd6fceb2dab51ea1c805454e537672fa
FRATAK4 <fratak4@gmail.com>
2025-02-28 14:18:49 +0100

    feat(file_handler_package): implements FileHandler class structure

---
+++ src/file_handler/file_handler.py
@@ -0,0 +1,22 @@
+from src.text import Text
+from typing import Dict
+
+
+class FileHandler:
+    def __init__(self) -> None:
+        self.path = ""
+
+    def set_path(self, name: str) -> None:
+        pass
+
+    def create_file(self) -> None:
+        pass
+
+    def delete_file(self) -> None:
+        pass
+
+    def save(self, texts: list[Text]) -> None:
+        pass
+
+    def load(self) -> list[Dict[str, str]]:
+        pass
