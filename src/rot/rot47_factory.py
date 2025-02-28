d75189056398d2046906a2f666d5f05ab21d8417
FRATAK4 <fratak4@gmail.com>
2025-02-28 15:31:10 +0100

    fix(rot_package): fixes class name (Rot13Factory -> Rot47Factory)

--- src/rot/rot47_factory.py
+++ src/rot/rot47_factory.py
@@ -2,6 +2,6 @@ from .rot_factory import RotFactory
 from .rot47_cipher import Rot47Cipher
 
 
-class Rot13Factory(RotFactory):
+class Rot47Factory(RotFactory):
     def create_cipher(self) -> Rot47Cipher:
         return Rot47Cipher()
