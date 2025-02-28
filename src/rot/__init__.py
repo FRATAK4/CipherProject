04e92ef7a2ad8b296decf54dbaafe16dd3730a5f
FRATAK4 <fratak4@gmail.com>
2025-02-28 15:33:57 +0100

    feat(rot_package): fills __init__.py file

---
+++ src/rot/__init__.py
@@ -0,0 +1,15 @@
+from .rot_factory import RotFactory
+from .rot13_factory import Rot13Factory
+from .rot47_factory import Rot47Factory
+from .rot_cipher import RotCipher
+from .rot13_cipher import Rot13Cipher
+from .rot47_cipher import Rot47Cipher
+
+__all__ = [
+    "RotFactory",
+    "Rot13Factory",
+    "Rot47Factory",
+    "RotCipher",
+    "Rot13Cipher",
+    "Rot47Cipher",
+]
