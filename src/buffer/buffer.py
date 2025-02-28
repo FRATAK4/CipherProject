78eda4a397dbcba5e65e218ab58f90c1b20bd006
FRATAK4 <fratak4@gmail.com>
2025-02-28 17:02:17 +0100

    refactor(buffer_package): refactor add_text method to add_texts method

--- src/buffer/buffer.py
+++ src/buffer/buffer.py
@@ -5,9 +5,10 @@ class Buffer:
     def __init__(self) -> None:
         self.texts: list[Text] = []
 
-    def add_text(self, text: Text) -> None:
-        if text not in self.texts:
-            self.texts.append(text)
+    def add_texts(self, texts: list[Text]) -> None:
+        for text in texts:
+            if text not in self.texts:
+                self.texts.append(text)
 
     def remove_text(self, number: int) -> None:
         self.texts.pop(number - 1)
