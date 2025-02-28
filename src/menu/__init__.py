43c176c626bbfdf0e95498e096713adc74ffc09f
FRATAK4 <fratak4@gmail.com>
2025-02-28 19:12:41 +0100

    feat(menu_package): fills __init__.py file

---
+++ src/menu/__init__.py
@@ -0,0 +1,19 @@
+from .create_file_menu import CreateFileMenu
+from .delete_file_menu import DeleteFileMenu
+from .encrypt_decrypt_menu import EncryptDecryptMenu
+from .load_texts_menu import LoadTextsMenu
+from .main_menu import MainMenu
+from .new_text_menu import NewTextMenu
+from .remove_text_menu import RemoveTextMenu
+from .save_texts_menu import SaveTextsMenu
+
+__all__ = [
+    "CreateFileMenu",
+    "DeleteFileMenu",
+    "EncryptDecryptMenu",
+    "LoadTextsMenu",
+    "MainMenu",
+    "NewTextMenu",
+    "RemoveTextMenu",
+    "SaveTextsMenu",
+]
