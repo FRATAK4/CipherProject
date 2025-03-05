acfec137b62dd167f432de74f1fc66632cb1d72b
FRATAK4 <fratak4@gmail.com>
2025-03-05 03:48:32 +0100

    feat(manager): develops manager class structure

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -7,7 +7,52 @@ class Manager:
     def __init__(self, file_handler: FileHandler, buffer: Buffer) -> None:
         self.file_handler = file_handler
         self.buffer = buffer
+        self.running = True
 
-    def run(self):
-        while True:
+        self._initialize_functions_dict()
+
+    def _initialize_functions_dict(self):
+        self.functions_dict = {
+            1: self._add_new_text,
+            2: self._remove_text,
+            3: self._empty_your_texts,
+            4: self._encrypt_decrypt_text,
+            5: self._save_texts_to_file,
+            6: self._load_texts_from_file,
+            7: self._create_file,
+            8: self._delete_file,
+            9: self._exit,
+        }
+
+    def run(self) -> None:
+        while self.running:
             MainMenu.show_menu(self.buffer)
+            user_input = MainMenu.get_input()
+            self.functions_dict.get(user_input)()
+
+    def _add_new_text(self):
+        pass
+
+    def _remove_text(self):
+        pass
+
+    def _empty_your_texts(self):
+        pass
+
+    def _encrypt_decrypt_text(self):
+        pass
+
+    def _save_texts_to_file(self):
+        pass
+
+    def _load_texts_from_file(self):
+        pass
+
+    def _create_file(self):
+        pass
+
+    def _delete_file(self):
+        pass
+
+    def _exit(self):
+        pass
