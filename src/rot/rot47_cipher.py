07b1646b11e6d9b202ef48d87cba783465f83282
FRATAK4 <fratak4@gmail.com>
2025-03-06 02:49:05 +0100

    feat(rot47_cipher): implements encrypt and decrypt methods

--- src/rot/rot47_cipher.py
+++ src/rot/rot47_cipher.py
@@ -3,8 +3,20 @@ from src.text import Text
 
 
 class Rot47Cipher(RotCipher):
-    def encrypt(self, text: Text) -> Text:
-        pass
+    def encrypt(self, text: Text) -> None:
+        encrypted_text = ""
+        for char in text.text:
+            encrypted_char = chr(33 + (ord(char) - 33 + 47) % 94)
+            encrypted_text += encrypted_char
 
-    def decrypt(self, text: Text) -> Text:
-        pass
+        text.text = encrypted_text
+        text.status = "encrypted"
+
+    def decrypt(self, text: Text) -> None:
+        decrypted_text = ""
+        for char in text.text:
+            decrypted_char = chr(33 + (ord(char) - 33 - 47) % 94)
+            decrypted_text += decrypted_char
+
+        text.text = decrypted_text
+        text.status = "decrypted"
