2b6dc4481d4daa5a888ca1009307e6d194195934
FRATAK4 <fratak4@gmail.com>
2025-03-06 19:45:56 +0100

    feat(exception_handling): implements raising exceptions to get_input in MainMenu method

--- src/menu/main_menu.py
+++ src/menu/main_menu.py
@@ -32,4 +32,10 @@ class MainMenu:
     @staticmethod
     def get_input() -> int:
         user_input = int(input("\nEnter a number of option: "))
+
+        if type(user_input) is not int:
+            raise TypeError
+        if user_input not in range(1, 10):
+            raise IndexError
+
         return user_input
