36f8a0d1eefc1b37d5b81b8c9ab96e26cd6b8bd9
FRATAK4 <fratak4@gmail.com>
2025-03-06 17:10:53 +0100

    feat(globals): automize files_list initialization

--- src/file_handler/globals.py
+++ src/file_handler/globals.py
@@ -1,1 +1,8 @@
-files_list = []
+import os
+
+from src.file_handler.consts import DIRECTORY_FOR_FILES_PATH
+
+files_list = [
+    file_name.removesuffix(".json")
+    for file_name in os.listdir(DIRECTORY_FOR_FILES_PATH)
+]
