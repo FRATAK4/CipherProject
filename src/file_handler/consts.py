a65fb1dd4133d1e960735b53a1dd4ae81435a386
FRATAK4 <fratak4@gmail.com>
2025-03-09 19:46:53 +0100

    refactor: replace absolute path with relative path

--- src/file_handler/consts.py
+++ src/file_handler/consts.py
@@ -1,1 +1,5 @@
-DIRECTORY_FOR_FILES_PATH = "files_for_texts"
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+DIRECTORY_FOR_FILES_PATH = os.path.join(script_dir, "files_for_texts")
--- src/file_handler/globals.py
+++ src/file_handler/globals.py
@@ -1,10 +1,6 @@
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-print(script_dir)
-
-
-DIRECTORY_FOR_FILES_PATH = os.path.join(script_dir, "files_for_texts")
+from src.file_handler.consts import DIRECTORY_FOR_FILES_PATH
 
 
 files_list = [
