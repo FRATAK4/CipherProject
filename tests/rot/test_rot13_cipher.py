cca3f73a5c44fe487593d7c9e4c4a54cef1e6d1c
FRATAK4 <fratak4@gmail.com>
2025-03-12 19:41:23 +0100

    refactor(test_rot13_cipher.py): deletes mock_ in fixtures

--- tests/rot/test_rot13_cipher.py
+++ tests/rot/test_rot13_cipher.py
@@ -5,30 +5,30 @@ from src.text import Text
 
 
 @pytest.fixture
-def mock_encrypted_text():
+def encrypted_text():
     return Text(text="apple", rot_type="rot13", status="encrypted")
 
 
 @pytest.fixture
-def mock_decrypted_text():
+def decrypted_text():
     return Text(text="hello", rot_type="rot13", status="decrypted")
 
 
 @pytest.fixture
-def mock_rot13_cipher_instance():
+def rot13_cipher_instance():
     factory = Rot13Factory()
     return factory.create_cipher()
 
 
 class TestRot13Cipher:
-    def test_encrypt(self, mock_rot13_cipher_instance, mock_decrypted_text):
-        mock_rot13_cipher_instance.encrypt(mock_decrypted_text)
-        assert mock_decrypted_text == Text(
+    def test_encrypt(self, rot13_cipher_instance, decrypted_text):
+        rot13_cipher_instance.encrypt(decrypted_text)
+        assert decrypted_text == Text(
             text="uryyb", rot_type="rot13", status="encrypted"
         )
 
-    def test_decrypt(self, mock_rot13_cipher_instance, mock_encrypted_text):
-        mock_rot13_cipher_instance.decrypt(mock_encrypted_text)
-        assert mock_encrypted_text == Text(
+    def test_decrypt(self, rot13_cipher_instance, encrypted_text):
+        rot13_cipher_instance.decrypt(encrypted_text)
+        assert encrypted_text == Text(
             text="nccyr", rot_type="rot13", status="decrypted"
         )
