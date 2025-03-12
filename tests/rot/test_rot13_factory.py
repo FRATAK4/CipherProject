743cc15f5c2c856a20afa218a9308f2e4a2aed0d
FRATAK4 <fratak4@gmail.com>
2025-03-12 19:44:14 +0100

    refactor(test_rot13_factory.py): deletes mock_ in fixtures

--- tests/rot/test_rot13_factory.py
+++ tests/rot/test_rot13_factory.py
@@ -4,10 +4,10 @@ from src.rot import Rot13Factory, Rot13Cipher
 
 
 @pytest.fixture
-def mock_rot13_factory_instance():
+def rot13_factory_instance():
     return Rot13Factory()
 
 
 class TestRot13Factory:
-    def test_create_cipher(self, mock_rot13_factory_instance):
-        assert isinstance(mock_rot13_factory_instance.create_cipher(), Rot13Cipher)
+    def test_create_cipher(self, rot13_factory_instance):
+        assert isinstance(rot13_factory_instance.create_cipher(), Rot13Cipher)
