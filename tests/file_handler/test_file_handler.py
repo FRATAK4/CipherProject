7c16239e768f44846577425dcc51b5ef75c1995a
FRATAK4 <fratak4@gmail.com>
2025-03-12 19:38:46 +0100

    refactor(test_file_handler.py): deletes mock_ in fixtures

--- tests/file_handler/test_file_handler.py
+++ tests/file_handler/test_file_handler.py
@@ -9,12 +9,12 @@ from src.text import Text
 
 
 @pytest.fixture
-def mock_file_handler_instance():
+def file_handler_instance():
     return FileHandler()
 
 
 @pytest.fixture
-def mock_text_objects1():
+def text_objects1():
     return [
         Text(text="hello1", rot_type="rot13", status="encrypted"),
         Text(text="hello2", rot_type="rot47", status="encrypted"),
@@ -22,7 +22,7 @@ def mock_text_objects1():
 
 
 @pytest.fixture
-def mock_text_objects2():
+def text_objects2():
     return [
         Text(text="hello2", rot_type="rot47", status="encrypted"),
         Text(text="hello3", rot_type="rot13", status="decrypted"),
@@ -30,7 +30,7 @@ def mock_text_objects2():
 
 
 @pytest.fixture
-def mock_text_objects3():
+def text_objects3():
     return [
         Text(text="hello1", rot_type="rot13", status="encrypted"),
         Text(text="hello2", rot_type="rot47", status="encrypted"),
@@ -39,59 +39,59 @@ def mock_text_objects3():
 
 
 class TestFileHandler:
-    def test_initialization(self, mock_file_handler_instance):
-        assert mock_file_handler_instance.path == ""
+    def test_initialization(self, file_handler_instance):
+        assert file_handler_instance.path == ""
 
-    def test_set_path(self, mock_file_handler_instance):
+    def test_set_path(self, file_handler_instance):
         with patch("src.file_handler.consts.DIRECTORY_FOR_FILES_PATH", "path"):
-            mock_file_handler_instance.set_path("name")
-        assert mock_file_handler_instance.path == "path/name.json"
+            file_handler_instance.set_path("name")
+        assert file_handler_instance.path == "path/name.json"
 
-    def test_create_file(self, mock_file_handler_instance):
-        mock_file_handler_instance.path = "file.json"
-        mock_file_handler_instance.create_file()
+    def test_create_file(self, file_handler_instance):
+        file_handler_instance.path = "file.json"
+        file_handler_instance.create_file()
         assert os.path.exists("file.json")
         if os.path.exists("file.json"):
             with open("file.json", "r") as file:
                 assert json.load(file) == []
             os.remove("file.json")
 
-    def test_delete_file(self, mock_file_handler_instance):
+    def test_delete_file(self, file_handler_instance):
         with open("file.json", "w") as file:
             json.dump([], file)
-        mock_file_handler_instance.path = "file.json"
-        mock_file_handler_instance.delete_file()
+        file_handler_instance.path = "file.json"
+        file_handler_instance.delete_file()
         assert not os.path.exists("file.json")
 
     def test_save(
         self,
-        mock_file_handler_instance,
-        mock_text_objects1,
-        mock_text_objects2,
-        mock_text_objects3,
+        file_handler_instance,
+        text_objects1,
+        text_objects2,
+        text_objects3,
     ):
         with open("file.json", "w") as file:
             json.dump([], file)
-        mock_file_handler_instance.path = "file.json"
-        mock_file_handler_instance.save(mock_text_objects1)
+        file_handler_instance.path = "file.json"
+        file_handler_instance.save(text_objects1)
         with open("file.json", "r") as json_file:
             texts = json.load(json_file)
         texts = [Text(**line) for line in texts]
-        assert texts == mock_text_objects1
+        assert texts == text_objects1
 
-        mock_file_handler_instance.save(mock_text_objects2)
+        file_handler_instance.save(text_objects2)
         with open("file.json", "r") as json_file:
             texts = json.load(json_file)
         texts = [Text(**line) for line in texts]
-        assert texts == mock_text_objects3
+        assert texts == text_objects3
         os.remove("file.json")
 
-    def test_load(self, mock_file_handler_instance, mock_text_objects1):
-        texts_as_dicts = [text.__dict__ for text in mock_text_objects1]
+    def test_load(self, file_handler_instance, text_objects1):
+        texts_as_dicts = [text.__dict__ for text in text_objects1]
         with open("file.json", "w") as file:
             json.dump(texts_as_dicts, file)
-        mock_file_handler_instance.path = "file.json"
-        texts = mock_file_handler_instance.load()
+        file_handler_instance.path = "file.json"
+        texts = file_handler_instance.load()
         texts = [Text(**line) for line in texts]
-        assert texts == mock_text_objects1
+        assert texts == text_objects1
         os.remove("file.json")
