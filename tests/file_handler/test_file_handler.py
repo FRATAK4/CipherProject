c89f07c83cfa53fd7750249f399b5108a41a3fd3
FRATAK4 <fratak4@gmail.com>
2025-03-12 16:06:21 +0100

    feat(test_file_handler.py): implements test_save and mock_text_objects fixtures

--- tests/file_handler/test_file_handler.py
+++ tests/file_handler/test_file_handler.py
@@ -5,11 +5,37 @@ from unittest.mock import patch
 import pytest
 
 from src.file_handler import FileHandler
+from src.text import Text
 
 
 @pytest.fixture
 def mock_file_handler_instance():
     return FileHandler()
+
+
+@pytest.fixture
+def mock_text_objects1():
+    return [
+        Text(text="hello1", rot_type="rot13", status="encrypted"),
+        Text(text="hello2", rot_type="rot47", status="encrypted"),
+    ]
+
+
+@pytest.fixture
+def mock_text_objects2():
+    return [
+        Text(text="hello2", rot_type="rot47", status="encrypted"),
+        Text(text="hello3", rot_type="rot13", status="decrypted"),
+    ]
+
+
+@pytest.fixture
+def mock_text_objects3():
+    return [
+        Text(text="hello1", rot_type="rot13", status="encrypted"),
+        Text(text="hello2", rot_type="rot47", status="encrypted"),
+        Text(text="hello3", rot_type="rot13", status="decrypted"),
+    ]
 
 
 class TestFileHandler:
@@ -36,3 +62,26 @@ class TestFileHandler:
         mock_file_handler_instance.path = "file.json"
         mock_file_handler_instance.delete_file()
         assert not os.path.exists("file.json")
+
+    def test_save(
+        self,
+        mock_file_handler_instance,
+        mock_text_objects1,
+        mock_text_objects2,
+        mock_text_objects3,
+    ):
+        with open("file.json", "w") as file:
+            json.dump([], file)
+        mock_file_handler_instance.path = "file.json"
+        mock_file_handler_instance.save(mock_text_objects1)
+        with open("file.json", "r") as json_file:
+            texts = json.load(json_file)
+        texts = [Text(**line) for line in texts]
+        assert texts == mock_text_objects1
+
+        mock_file_handler_instance.save(mock_text_objects2)
+        with open("file.json", "r") as json_file:
+            texts = json.load(json_file)
+        texts = [Text(**line) for line in texts]
+        assert texts == mock_text_objects3
+        os.remove("file.json")
